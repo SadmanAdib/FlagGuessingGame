fca6b82a3789dc4c3bf4afefd43bd532728ab59d
Sadman Adib <sadmanadib67@gmail.com>
2022-03-01 00:22:04 +0600

    Initial Commit

---
+++ FlagGuessingGame/ContentView.swift
@@ -0,0 +1,134 @@
+//
+//  ContentView.swift
+//  FlagGuessingGame
+//
+//  Created by Sadman Adib on 2/19/22.
+//
+
+import SwiftUI
+
+//creating custom ViewModifier with View extension for a title
+struct Title: ViewModifier{
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding()
+        
+    }
+    
+}
+
+extension View{
+    func titleStyle() -> some View{
+        modifier(Title())
+    }
+}
+
+//for replacing Image view with FlagImage view
+struct FlagImage: View{
+    var name: String
+    
+    var body: some View{
+        Image(name)
+            .renderingMode(.original).clipShape(Capsule()).shadow(radius: 5)
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var showNumber = 0
+    @State private var showFinalScore = false
+    @State private var questionNumber = 0
+    
+    @State private var countries = ["afghanistan", "albania", "algeria", "andorra", "angola", "antigua", "argentina", "bahamas", "bahrain", "bangladesh"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    var body: some View {
+        
+        ZStack{
+            
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+            
+            
+            VStack( spacing: 30){
+                VStack{
+                    Text("Flag Guessing Game").titleStyle()
+                    Text("Tap the correct flag of").foregroundColor(.white).font(.subheadline.weight(.heavy))
+                    Text(countries[correctAnswer]).foregroundColor(.white).font(.largeTitle.weight(.semibold))
+                }
+                
+                ForEach(0..<3){ number in
+                    Button{
+                        flagTapped(number)
+                    } label: {
+                        FlagImage(name: countries[number])
+//                        Image(countries[number])
+//                            .renderingMode(.original).clipShape(Capsule()).shadow(radius: 5)
+                    }
+                }
+            }
+        }.alert(scoreTitle, isPresented: $showingScore){
+            Button("continue", action: askQuestion)
+        }message:{
+            if scoreTitle == "Wrong!"{
+                Text("That's the flag of \(countries[showNumber])")
+            }
+            else{
+                Text("Your score is \(score)")
+            }
+        }.alert(scoreTitle, isPresented: $showFinalScore){
+            Button("Restart", action: resetGame)
+        }message:{
+            Text("Your final score is \(score + 1) out of 8")
+        }
+    }
+    
+    func resetGame(){
+        score = 0
+        questionNumber = 0
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func flagTapped(_ number : Int) {
+        if number == correctAnswer && questionNumber != 7{
+            scoreTitle = "Correct!"
+            score+=1
+            showingScore = true
+        }
+        else if number != correctAnswer && questionNumber != 7{
+            scoreTitle = "Wrong!"
+            showNumber = number
+            showingScore = true
+        }
+        else{
+            showingScore = false
+            showFinalScore = true
+        }
+        
+        questionNumber+=1
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlagGuessingGame/FlagGuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessingGameApp.swift
+//  FlagGuessingGame
+//
+//  Created by Sadman Adib on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
